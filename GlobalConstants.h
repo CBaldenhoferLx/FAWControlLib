0156a01ed8c770535894744b4d53d179e6e4bdfe
B1 <cbstar@web.de>
2018-10-20 13:10:11 +0200

    initial version

---
+++ GlobalConstants.h
@@ -0,0 +1,7 @@
+
+#define LEVEL_COUNT 3
+
+#define FAN_SPEED_MAX 9
+#define FAN_SPEED_MIN 0
+
+#define MM_COUNT 3
---
+++ Protocol.h
@@ -0,0 +1,39 @@
+#define DATA_PACKAGE_SIZE 4
+
+// 1st byte: identifier
+#define CMD_IDENTIFIER '@'
+
+// 2nd byte: command, capital: feedback going up
+#define CMD_PING 'p'
+#define CMD_PING_FB 'P'
+
+#define CMD_VAPO 'v'
+#define CMD_VAPO_FB 'V'
+#define CMD_MM_FB 'M'
+
+#define CMD_FAN 'f'
+#define CMD_FAN_FB 'F'
+
+#define CMD_SEAT 's'
+#define CMD_LEVEL_FB 'L'
+
+// 3rd byte: modifier or index as char
+#define MOD_LEFT 'l'
+#define MOD_MIDDLE 'm'
+#define MOD_RIGHT 'r'
+#define MOD_NONE 'x'
+
+#define CMD_FORWARD 'f'
+#define CMD_BACKWARD 'b'
+
+#define INDEX_TO_LOCATION_MOD(index)        										\
+(                                       											\
+  ((index==0)?MOD_LEFT:(index==1)?MOD_MIDDLE:(index==2)?MOD_RIGHT:MOD_NONE)			\
+)
+
+#define LOCATION_MOD_TO_INDEX(loc)        										\
+(                                       										\
+  ((loc==MOD_LEFT)?0:(loc==MOD_MIDDLE)?1:(loc==MOD_RIGHT)?2:-1)					\
+)
+
+// 4th param: (value as char)
