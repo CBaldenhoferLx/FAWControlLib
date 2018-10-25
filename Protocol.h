c8d7a018087552f35d7f332a5d3780597ec5bc92
B1 <cbstar@web.de>
2018-10-25 19:58:46 +0200

    seat

--- Protocol.h
+++ Protocol.h
@@ -15,6 +15,9 @@
 #define CMD_FAN_FB 'F'
 
 #define CMD_SEAT 's'
+#define CMD_SEAT_POS_FB 'S'
+#define CMD_SEAT_SWITCH_FB 'W'
+#define CMD_SEAT_MOVE_FB 'O'
 
 #define CMD_LEVEL_FB 'L'
 
@@ -25,21 +28,24 @@
 #define CMD_LED_BRIGHTNESS_FB 'B'
 
 // 3rd byte: modifier or index as char
+#define MOD_NONE 'x'
+#define MOD_ERROR 'e'
+
 #define MOD_LEFT 'l'
 #define MOD_MIDDLE 'm'
 #define MOD_RIGHT 'r'
-#define MOD_NONE 'x'
 
 #define MOD_LED_DASHBOARD 'd'
 #define MOD_LED_FINS 'f'
 #define MOD_LED_CABLE_HOLDER 'c'
 
-#define CMD_FORWARD 'f'
-#define CMD_BACKWARD 'b'
+#define MOD_STOP 's'
+#define MOD_FORWARD 'f'
+#define MOD_BACKWARD 'b'
 
 #define INDEX_TO_LOCATION_MOD(index)        										\
 (                                       											\
-  ((index==0)?MOD_LEFT:(index==1)?MOD_MIDDLE:(index==2)?MOD_RIGHT:MOD_NONE)			\
+  ((index==0)?MOD_LEFT:(index==1)?MOD_MIDDLE:(index==2)?MOD_RIGHT:MOD_ERROR)			\
 )
 
 #define LOCATION_MOD_TO_INDEX(loc)        										\
@@ -49,7 +55,17 @@
 
 #define INDEX_TO_LED_MOD(index)        																		\
 (                                       																	\
-  ((index==0)?MOD_LED_DASHBOARD:(index==1)?MOD_LED_FINS:(index==2)?MOD_LED_CABLE_HOLDER:MOD_NONE)			\
+  ((index==0)?MOD_LED_DASHBOARD:(index==1)?MOD_LED_FINS:(index==2)?MOD_LED_CABLE_HOLDER:MOD_ERROR)			\
+)
+
+#define SEAT_POS_TO_STRING(pos)        																		\
+(                                       																	\
+  ((pos==0)?F("Moving"):(pos==1)?F("Forward"):(pos==2)?F("Backward"):F("Unknown"))			\
+)
+
+#define SEAT_MOVE_TO_STRING(pos)        																		\
+(                                       																	\
+  ((pos==0)?F("Stop"):(pos==1)?F("Forward"):(pos==2)?F("Backward"):(pos==3)?F("Error"):F("Unknown"))			\
 )
 
 // 4th param: (value as char)
